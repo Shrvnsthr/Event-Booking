6f60188876399757f63b1d7ce01b58d19c380f00
Shravan <sharvansuthar234@gmail.com>
2025-06-22 12:18:23 +0530

    Added updates for navbar and dark mode

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -5,6 +5,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // 👈 Add this line
   theme: {
     extend: {
       colors: {
